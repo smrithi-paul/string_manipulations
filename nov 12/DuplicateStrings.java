b0f7a4c83145958e445d646dbbb07dc131a10b5f
smrithi-paul <74285484+smrithi-paul@users.noreply.github.com>
2020-11-13 19:06:47 +0530

    Create DuplicateStrings.java

---
+++ nov 12/DuplicateStrings.java
@@ -0,0 +1,35 @@
+import java.util.HashMap; 
+import java.util.Map; 
+import java.util.Set;
+
+public class DuplicateStrings {
+     public static void main(String[] args) {
+  	String str = "Java is Object Oriented";
+
+        //Converts given string into character array  
+        char charArray[] = str.toCharArray();            
+          
+        System.out.println("Given string: "+str);  
+
+        //create a hashmap
+	Map<Character, Integer> charMap = new HashMap<Character, Integer>();
+	//loop each characters and put to hashmap with no:of times duplicate occurred
+	for(Character ch: charArray) {
+		if(charMap.containsKey(ch)) {
+			charMap.put(ch, charMap.get(ch)+1);
+		} else {
+			if(!(ch==' ')) { 
+				charMap.put(ch,1);
+			}
+		}
+	}        
+
+	//Print hashMap with duplicates
+	System.out.println("Duplicate characters are: ");
+	for(Map.Entry<Character,Integer> charEntry : charMap.entrySet()) {
+		if(charEntry.getValue()>1) {
+			System.out.println(charEntry.getKey()+" : "+charEntry.getValue());
+		}
+	} 
+    }  
+ }
